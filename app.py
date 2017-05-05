c7902b4e578eecdd10543e6cedfe91be725c8008
jasonmoore002 <jason@andymoore.biz>
2017-05-05 20:51:20 +0100

    Update app.py
    Adds functions to change the title and dimensions of the screen at will

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+from os import system
+
 class app():
-	def __init__(rows, cols):
-		system('mode con: cols='+ str(cols) + ' lines='+ str(rows+1))
+	def __init__(self, width=100, height=35, title="changeme"):
+		self.setTitle(title)
+		self.setSize(width, height)
+		self.active = False
+	
+	def setTitle(self, newTitle):
+		self.title = newTitle
+		system("title " + str(self.title))
+	
+	def setSize(self, width, height):
+		self.width = width
+		self.height = height
+		system("mode con: cols=" + str(self.width) + " lines=" + str(self.height))
